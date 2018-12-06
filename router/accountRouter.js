560f1ba851c65a1fdb61233b4ec919027225c80b
voitovych <chumba.vumbaa@gmail.com>
2018-12-06 03:47:44 +0200

    Twitter login added

--- router/accountRouter.js
+++ router/accountRouter.js
@@ -23,4 +23,29 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.put("/", async (req, res) => {
+  const accessToken = req.body.accessToken;
+  const accessSecret = req.body.accessSecret;
+  const twitterLink = req.body.twitterLink;
+  const twitterUsername = req.body.twitterUsername;
+  const twitterScreenName = req.body.twitterScreenName;
+
+  const resultUser = await db.updateUser(req.app.locals.userId, accessToken, accessSecret, twitterLink, twitterUsername, twitterScreenName)
+    .catch(error => {
+      switch (error.code) {
+        default: {
+          res.status(500).send({
+            code: 500,
+            status: "INTERNAL_SERVER_ERROR",
+            message: "Internal server error"
+          });
+        }
+      }
+    });
+
+  if (typeof resultUser !== "undefined") {
+    res.status(200).send(converter.userShortObjToJson(resultUser));
+  }
+});
+
 module.exports = router;
