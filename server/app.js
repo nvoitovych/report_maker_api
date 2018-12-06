58b5b2a6258be59baf683995de69f33cf260f12f
voitovych <chumba.vumbaa@gmail.com>
2018-12-06 02:13:06 +0200

    CORS added, added GET /public-api/routes method and bug fixed

--- server/app.js
+++ server/app.js
@@ -10,7 +10,12 @@ app.use(bodyParser.json());
 const publicRouter = require("../router/publicRouter");
 const adminRouter = require("../router/adminRouter");
 const privateRouter = require("../router/privateRouter");
+const CORS = require("../middleware/CORS");
 
+app.use(CORS);
+app.get("/routes", (req, res) => {
+  return res.status(200).send({"routes: ": app._router.stack});
+});
 app.use("/public-api", publicRouter);
 app.use("/admin-api", adminRouter);
 app.use("/private-api", privateRouter);
